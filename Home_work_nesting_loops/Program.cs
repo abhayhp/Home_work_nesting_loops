8667294a9af88593680e8a39846ba3b4a1f5987d
abhayhp <dadhwal.abhay.abhay@gmail.com>
2022-09-11 10:36:41 +0530

    Add project files.

---
+++ Home_work_nesting_loops/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_work_nesting_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1)
+            //for ( int i = 1; i <= 5; i++)
+            //{
+            //    for ( int j = 1; j <= i; j++)
+            //    {
+
+            //        Console.Write(j);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //2)
+            //for( int i = 1; i<= 5; i++)
+            //{
+            //    for ( int j = 5; j >= i; j--)
+            //    {
+            //        Console.Write(j);
+
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //3)
+            //for ( int i = 1; i<= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(Convert.ToChar('A' + (j - 1)));
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //4)
+            //for ( int i = 5; i>= 1; i--)
+            //{
+            //    for ( int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(j);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //5)
+            //for ( int i = 1; i <=5; i++)
+            //{
+            //    for (int k = 5; k >= i; k--)
+            //    {
+            //        Console.Write(" ");
+
+            //    }
+            //    for( int j = 1; j<= i; j++)
+            //    {
+            //        Console.Write("* ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            
+        }
+    }
+}
